62b13519abdf9d452abf503c25aec1a265d23d8f
Caleb Owens <caleb.ja.owens@gmail.com>
2019-12-27 12:17:33 -0500

    Fixed permissions and almost done with PlayerCommands.java. Working on sendhelp method

--- src/me/caleb/BlockBR/commands/PlayerCommands.java
+++ src/me/caleb/BlockBR/commands/PlayerCommands.java
@@ -5,6 +5,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginManager;
 
 import me.caleb.BlockBR.Main;
 import me.caleb.BlockBR.admin.Rewards;
@@ -20,7 +22,6 @@ public class PlayerCommands implements CommandExecutor{
 	public PlayerCommands(Main plugin) {
 		this.plugin = plugin;
 		plugin.getCommand("blockbr").setExecutor(this);
-		plugin.getCommand("blockbra").setExecutor(this);
 	}
 	
 	@Override
@@ -42,46 +43,42 @@ public class PlayerCommands implements CommandExecutor{
 				Chat.sendConsoleMessage("You must be a player!");
 			}else {
 				
-				//Args[1] should be the tierName
-				if(args[0].equalsIgnoreCase("tierlist")) {
-					if(isPlayer(player) == true) {
+				if(args[0].equalsIgnoreCase("tierlist") && args.length == 1) {
+					if(isPlayer() == true) {
 						t.showTierList(player);
-					}
-				}	
-				
+					}	
 				//0 is tier, 1 is add or remove, 2 is tiername
-				if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("add") && !args[2].isEmpty()) {
-					if(isAdmin(player) == true) {
+				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("add") && !args[2].isEmpty()) {
+					if(isAdmin() == true) {
 						t.tierAdd(args[2], player);
 					}
 				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("remove") && !args[2].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						if(args[2].equalsIgnoreCase("all")) {
 							t.tierRemove(player);
 						}else {
 							t.tierRemove(args[2], player);
-						}
-						
+						}	
 					}		
 				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("edit") && !args[2].isEmpty() && !args[3].isEmpty() && !args[4].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						t.tierEdit(args[2], args[3], args[4], player);
 						t.configWork(args[2], args[1]);
 					}
 				//Crates
 				//bbr rewardadd (tier) ("Crate") (CrateName)
 				}else if(args[0].equalsIgnoreCase("rewardedit") && !args[1].isEmpty() && args[2].equalsIgnoreCase("crate") && !args[3].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.rewardAddCrate(player, args[1], args[2], args[3]);
 					}
 				//Items
 				//bbr rewardadd (tier) ("Item")
 				}else if(args[0].equalsIgnoreCase("rewardadd") && !args[1].isEmpty() && args[2].equalsIgnoreCase("Item")) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.rewardAddItem(player, args[1]);
 					}
 				}else if(args[0].equalsIgnoreCase("rewardedit") && !args[1].isEmpty() && args[2].equalsIgnoreCase("Money") && !args[3].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						try {
 							r.rewardAddMoney(player, args[1], args[2],Integer.parseInt(args[3]));
 						}catch(NumberFormatException e) {
@@ -90,96 +87,100 @@ public class PlayerCommands implements CommandExecutor{
 					}
 				//bbr tier rename (tier) (newName)
 				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("rename") && !args[2].isEmpty() && !args[3].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						t.tierRename(args[2], args[3],player);
 					}
 				}else if(args[0].equalsIgnoreCase("rewardadd") && !args[1].isEmpty() && args[2].equalsIgnoreCase("command")) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.rewardAddCommand(player, args[1]);
 					}
 				//bbr tier info
 				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("info") && !args[2].isEmpty()) {
-					if(isPlayer(player) == true) {
+					if(isPlayer() == true) {
 						t.getTierInfo(player, args[2]);
 					}
 				//bbr rewardremove (Tier) "Item" (##)
 				}else if(args[0].equalsIgnoreCase("rewardremove") && !args[1].isEmpty() && args[2].equalsIgnoreCase("Item") && !args[3].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.rewardRemoveItem(args[1],args[3],player);
 					}
 				//bbr rewardremove (Tier) "Command" (##)
 				}else if(args[0].equalsIgnoreCase("rewardremove") && !args[1].isEmpty() && args[2].equalsIgnoreCase("Command") && !args[3].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.rewardRemoveCommand(args[1], args[3], player);
 					}
 				//bbr changeType (Type)
 				}else if(args[0].equalsIgnoreCase("changeType") && !args[1].isEmpty()) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						r.changeMineType(args[1], player);
+					}else {
+						return false;
 					}
 				//bbr group create (Group Name)
 				}else if(args[0].equalsIgnoreCase("group") && args[1].equalsIgnoreCase("create") && args.length == 3) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						g.createGroup(args[2], player);
 					}
 				//bbr group remove (Group Name)
 				}else if(args[0].equalsIgnoreCase("group") && args[1].equalsIgnoreCase("remove") && args.length == 3) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						g.removeGroup(args[2], player);
 					}
 				//bbr tier move (Group Name)
 				}else if(args[0].equalsIgnoreCase("tier") && args[1].equalsIgnoreCase("move") && args.length == 4) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						player.sendMessage("here");
 						t.tierMove(args[2], args[3]);
 					}
 				}else if(args[0].equalsIgnoreCase("groupList") && args.length == 1) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						g.showGroupList();
 					}
 				}else if(args[0].equalsIgnoreCase("group") && args[1].equalsIgnoreCase("tiers") && args.length == 3) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						g.groupTiers(args[2]);
 					}
 				}else if(args[0].equalsIgnoreCase("group") && args[1].equalsIgnoreCase("rename") && args.length == 4) {
-					if(isAdmin(player) == true) {
+					if(isAdmin() == true) {
 						g.editName(args[2], args[3]);
 					}
+				}else {
+					sendHelp();
 				}	
 
 			}
 			
 		}catch(ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
+			sendHelp();
 		}
 		
 		return false;
 	}
 	
-	public boolean isAdmin(Player p) {
+	public boolean isAdmin() {
 		
-		if(p.hasPermission("blockbr.admin")) {
+		if(player.hasPermission("blockbr.admin")) {
 			return true;
 		}else {
-			Chat.sendPlayerMessage(p, "You do not have permission to use this command!");
+			Chat.sendPlayerMessage(player, "You do not have permission to use this command!");
 			return false;
 		}
 		
 	}
 	
-	public boolean isPlayer(Player p) {
+	public boolean isPlayer() {
 		
-		if(p.hasPermission("blockbr.use")) {
+		if(player.hasPermission("blockbr.use")) {
 			return true;
 		}else {
-			Chat.sendPlayerMessage(p, "You do not have permission to use this command!");
+			Chat.sendPlayerMessage(player, "You do not have permission to use this command!");
 			return false;
 		}
 		
 	}
 	
 	public void sendHelp() {
-		
+		Chat.sendPlayerMessage(player, "List of commands:");	
 	}
 
 }
---
+++ src/me/caleb/BlockBR/commands/TabCompletion.java
@@ -0,0 +1,17 @@
+package me.caleb.BlockBR.commands;
+
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class TabCompletion implements TabCompleter{
+
+	@Override
+	public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
