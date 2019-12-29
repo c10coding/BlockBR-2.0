1e962eb9fd3a854f32f0b28d171a6a753294888f
Caleb <caleb.ja.owens@gmail.com>
2019-12-29 10:02:15 -0500

    Little bit of work done on the menus

--- src/me/caleb/BlockBR/menus/AbstractMenu.java
+++ src/me/caleb/BlockBR/menus/AbstractMenu.java
@@ -1,12 +1,16 @@
 package me.caleb.BlockBR.menus;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -30,24 +34,116 @@ public abstract class AbstractMenu implements Listener, InventoryHolder{
 		return inv;
 	}
 	
+	public void openInventory(Player p) {
+		p.openInventory(inv);
+	}
+	
 	/*
 	 * If the item has enchantments
 	 */
 	public static ItemStack createGuiItem(Material mat, String name, Map<Enchantment,Integer> enchants, int amount, String...lore) {
 		
 		ItemStack item = new ItemStack(mat,1);
+		ItemMeta meta = item.getItemMeta();
+		ArrayList<Enchantment> itemEnchants = new ArrayList<Enchantment>();
+		ArrayList<Integer> itemLevels = new ArrayList<Integer>();
+		
+		//Gets all the enchants from the map
+		for(Entry<Enchantment, Integer> en : enchants.entrySet()){
+			itemEnchants.add(en.getKey());
+		}
+		
+		//Gets all the levels from the map
+		for(Entry<Enchantment, Integer> en : enchants.entrySet()){
+			itemLevels.add(en.getValue());
+		}
 		
 		for(int x = 0; x < enchants.size(); x++) {
 			
 			if(!item.getType().equals(Material.NETHER_STAR)) {
-				ItemMeta meta = item.getItemMeta();
+				meta.addEnchant(itemEnchants.get(x), itemLevels.get(x), true);
 			}else {
 				EnchantmentStorageMeta metaE = (EnchantmentStorageMeta) item.getItemMeta();
-				metaE.addStoredEnchant(, arg1, arg2)
+				metaE.addStoredEnchant(itemEnchants.get(x), itemLevels.get(x), true);
+				item.setItemMeta(metaE);
 			}
 			
 		}
 		
-		return null;
+		meta.setDisplayName(name);
+		item.setAmount(amount);
+		
+		ArrayList<String> metaLore = new ArrayList<String>();
+		
+		for(String lorecomments : lore) {
+			metaLore.add(lorecomments);
+		}
+		
+		if(!item.getType().equals(Material.ENCHANTED_BOOK)) {
+			meta.setLore(metaLore);
+			item.setItemMeta(meta);
+		}
+		
+		return item;
 	}
+	
+	/*
+	 * No enchants. Regular items
+	 */
+	public static ItemStack createGuiItem(Material material, String name,int amount, String...lore) {
+		
+		ItemStack item = new ItemStack(material,1);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		item.setAmount(amount);
+		ArrayList<String> metaLore = new ArrayList<String>();
+		
+		for(String lorecomments : lore) {
+			
+			metaLore.add(lorecomments);
+			
+		}
+		
+		meta.setLore(metaLore);
+		item.setItemMeta(meta);
+		return item;
+		
+	}
+	
+	//If you only want one item
+	public static ItemStack createGuiItem(Material material, String name, String...lore) {
+		
+		ItemStack item = new ItemStack(material,1);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		
+		ArrayList<String> metaLore = new ArrayList<String>();
+		
+		for(String lorecomments : lore) {
+			
+			metaLore.add(lorecomments);
+			
+		}
+		
+		meta.setLore(metaLore);
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+	//Filler item
+	public static ItemStack createGuiItem() {
+		
+		ItemStack item = new ItemStack(Material.CYAN_STAINED_GLASS_PANE,1);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName("");
+		meta.setLore(null);
+		
+		return item;
+	}
+	
+	public abstract void initializeItems(String tier, int amount, int level, double threshold);
+	
+	@EventHandler
+    public abstract void onInventoryClick(InventoryClickEvent e);
+	
 }
